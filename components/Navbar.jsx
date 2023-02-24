e5e27dc58cdf90c7c431914e14a87833095adf57
starling <starling@starlings-MBP.lan>
2023-02-24 08:47:11 -0600

    &apos;

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -136,7 +136,7 @@ const Navbar = () => {
 
             <div className="pt-20">
               <p className="uppercase tracking-widest text-[#5651e5]">
-                Let's Connect
+                Let&apos;s Connect
               </p>
               <div className=" flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
