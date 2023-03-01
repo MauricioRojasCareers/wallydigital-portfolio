f216b220cc4516d3601b8e4ab3c911075100a321
starling <starling@starlings-MBP.lan>
2023-02-28 22:48:15 -0600

    next-cloudinary integration

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Cloudinary } from "@cloudinary/url-gen";
 import { AdvancedVideo } from "@cloudinary/react";
+import { CldVideoPlayer } from "next-cloudinary";
 
 const cld = new Cloudinary({
   cloud: {
@@ -15,13 +16,10 @@ const Main = () => {
   return (
     <>
       <div className="w-full h-screen bg-gray-200 flex justify-center items-center fixed cursor-pointer">
-        <AdvancedVideo
-          cldVid={myVideo}
-          autoplay="true"
-          muted
-          loop
-          className="absolute z-10 w-auto 
-min-w-full min-h-full max-w-none"
+        <CldVideoPlayer
+          width="1920"
+          height="1080"
+          src="portfolio-videos/bg-final"
         />
         {/* <video
           src={}
@@ -35,7 +33,7 @@ min-w-full min-h-full max-w-none
 "
         ></video> */}
 
-        <Link
+        {/* <Link
           href="/"
           className="absolute inset-0 flex justify-center items-center z-10"
         >
@@ -47,7 +45,7 @@ min-w-full min-h-full max-w-none
             height={500}
             priority={true}
           ></Image>
-        </Link>
+        </Link> */}
       </div>
     </>
   );
